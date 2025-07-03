8621aa60a8a748b8ee1554d712bdff9b3438356c
subhanan.kar@if.se <subhanan.kar@if.se>
2025-07-03 17:41:13 +0530

    Standard Route test passed

--- src/test/java/rebel2/StarshipTest.java
+++ src/test/java/rebel2/StarshipTest.java
@@ -18,4 +18,17 @@ class StarshipTest {
         Assertions.assertEquals(0, result, "Expected 0");
     }
 
+    @Test
+    public void shouldCalculateStandardRoute() {
+        //GIVEN: a starship has MGLT value 75, and distance is 750
+        Starship starship = new Starship(75);
+        double distance = 750;
+
+        //WHEN: plan the trip
+        double result = starship.calculate(distance);
+
+        //THEN: expect calculated travel time to be 10 hours
+        Assertions.assertEquals(10, result, "Expected 10 hours for 750 MGLT at 75 MGLT speed");
+    }
+
 }
